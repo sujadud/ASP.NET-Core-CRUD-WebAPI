21947086a88f205f0da8aa8bfbdc37ff24165801
Sujad Ud Doula <sujad.ud@outlook.com>
2023-05-19 02:21:17 +0600

    Hello

---
+++ CrudAPI/Models/ContactDbEintities.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI.Models
+{
+    public class ContactDbEintities : DbContext
+    {
+        public ContactDbEintities(DbContextOptions options) : base(options) { }
+        public DbSet<Contacts> Contacts { get; set; }
+    }
+}
---
+++ CrudAPI/Models/Contacts.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudAPI.Models
+{
+    public class Contacts
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
